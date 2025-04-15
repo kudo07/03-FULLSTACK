bc69b50efb87edbea853b294c23253643764671a
kudo07 <vknwl077@gmail.com>
2025-04-15 05:30:43 +0530

    complete

--- 3-WEEK-4/4.3-EXPRESS/CODE/index.js
+++ 3-WEEK-4/4.3-EXPRESS/CODE/index.js
@@ -1,15 +1,12 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 const app = express();
-
+app.use(bodyParser.json());
 const users = [
   //   0
   {
     name: 'john',
-    kidneys: [
-      {
-        healthy: false,
-      },
-    ],
+    kidneys: [{ healthy: true }, { healthy: false }, { healthy: true }],
   },
 ];
 
@@ -34,6 +31,29 @@ app.get('/', (req, res) => {
 });
 
 // post
+app.post('/', (req, res) => {
+  const isHealthy = req.body.isHealthy;
+  users[0].kidneys.push({
+    healthy: isHealthy,
+  });
+  res.json({ msg: 'done' });
+});
+
+// update
+app.put('/', (req, res) => {
+  for (let i = 0; i < users.kidneys.length; i++) {
+    users[0].kidneys[i].healthy = true;
+  }
+  res.json({});
+});
+app.delete('/', (req, res) => {
+  const healthyKidneys = users[0].kidneys.filter(
+    (kidney) => kidney.healthy === true
+  );
+  console.log(healthyKidneys);
+  users[0].kidneys = healthyKidneys;
+  res.json('done');
+});
 
 app.listen(3000, (req, res) => {
   console.log('server running');
