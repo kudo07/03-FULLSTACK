3bd25eb48f01f401db813923803671c70482f5d7
kudo07 <vknwl077@gmail.com>
2025-08-03 23:31:02 +0530

    backend-common

---
+++ 21-WEEK-22/22.1-EXALIODRAW-1/CODE/exclioClon/apps/ws-backend/src/index.ts
@@ -0,0 +1,23 @@
+import { WebSocketServer } from 'ws';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { JWT_SECRET } from './config';
+const wss = new WebSocketServer({ port: 8082 });
+
+wss.on('connection', function connection(ws, request) {
+  ws.on('message', function message(data) {
+    const url = request.url;
+    // we dont want unauthenticated users to connect our servers
+    const queryParams = new URLSearchParams(url?.split('?')[1]);
+    const token = queryParams.get('token') || '';
+    const decoded = jwt.verify(token, JWT_SECRET);
+    if (typeof decoded === 'string') {
+      ws.close();
+      return;
+    }
+    if (!decoded || !(decoded as JwtPayload).userId) {
+      ws.close();
+      return;
+    }
+    ws.send('pong');
+  });
+});
