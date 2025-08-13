e712044d4749654ce31c2a6ee3ae51d8ff51fe8f
kudo07 <vknwl077@gmail.com>
2025-08-14 00:07:39 +0530

    signup

--- 21-WEEK-22/22.1-EXALIODRAW-1/CODE/exclioClon/apps/http-backend/src/index.ts
+++ 21-WEEK-22/22.1-EXALIODRAW-1/CODE/exclioClon/apps/http-backend/src/index.ts
@@ -1,5 +1,33 @@
+import { CreateUserSchema } from '@repo/common/types';
 import express from 'express';
-
+import { prismaClient } from '@repo/db/client';
 const app = express();
+app.use(express.json());
+
+app.post('/signup', async (req, res) => {
+  const parsedData = CreateUserSchema.safeParse(req.body);
+
+  if (!parsedData.success) {
+    console.log(parsedData.error);
+    res.json({
+      message: 'Incorrect inputs',
+    });
+    return;
+  }
+  try {
+    const user = await prismaClient.user.create({
+      data: {
+        email: parsedData.data?.username,
+        password: parsedData.data.password,
+        name: parsedData.data.name,
+      },
+    });
+    res.json({
+      userId: user.id,
+    });
+  } catch (error) {
+    res.status(411).json({ message: 'User already exists with this username' });
+  }
+});
 
 app.listen(3030);
--- 21-WEEK-22/22.1-EXALIODRAW-1/CODE/exclioClon/apps/ws-backend/src/index.ts
+++ 21-WEEK-22/22.1-EXALIODRAW-1/CODE/exclioClon/apps/ws-backend/src/index.ts
@@ -1,7 +1,7 @@
 import { WebSocketServer } from 'ws';
 import jwt, { JwtPayload } from 'jsonwebtoken';
-import { JWT_SECRET } from '../../../packages/backend-common/dist/index.js';
-
+// import { JWT_SECRET } from '../../../packages/backend-common/dist/index.js';
+import { JWT_SECRET } from '@repo/backend-common/config';
 const wss = new WebSocketServer({ port: 8082 });
 
 wss.on('connection', function connection(ws, request) {
